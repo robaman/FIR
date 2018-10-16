21db0b49306090d72e305f48c55b8dd75d55353f
Robert Kästel <robert@kastel.se>
2018-10-16 13:48:16 +0200

    Set page size to 10000.

--- fir_api/settings.py
+++ fir_api/settings.py
@@ -2,5 +2,5 @@
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication', 'rest_framework.authentication.SessionAuthentication'),
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10000
 }
